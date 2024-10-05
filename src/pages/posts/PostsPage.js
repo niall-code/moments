0207ec2ff4c20d2bba1922041c9a1ca7d765f437
Niall Caplis <niallcaplis@gmail.com>
2024-10-05 15:07:11 +0000

    Infinite Scroll

--- src/pages/posts/PostsPage.js
+++ src/pages/posts/PostsPage.js
@@ -15,6 +15,9 @@ import { axiosReq } from "../../api/axiosDefaults";
 
 import NoResults from "../../assets/no-results.png";
 
+import InfiniteScroll from "react-infinite-scroll-component";
+import { fetchMoreData } from "../../utils/utils";
+
 function PostsPage({ message, filter = "" }) {
     const [posts, setPosts] = useState({ results: [] });
     const [hasLoaded, setHasLoaded] = useState(false);
@@ -60,9 +63,15 @@ function PostsPage({ message, filter = "" }) {
                 {hasLoaded ? (
                     <>
                         {posts.results.length ? (
-                            posts.results.map((post) => (
-                                <Post key={post.id} {...post} setPosts={setPosts} />
-                            ))
+                            <InfiniteScroll
+                                children={posts.results.map((post) => (
+                                    <Post key={post.id} {...post} setPosts={setPosts} />
+                                ))}
+                                dataLength={posts.results.length}
+                                loader={<Asset spinner />}
+                                hasMore={!!posts.next}
+                                next={() => fetchMoreData(posts, setPosts)}
+                            />
                         ) : (
                             <Container className={appStyles.Content}>
                                 <Asset src={NoResults} message={message} />
